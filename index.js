2933b1a33e542c01566b3935ca766a9d1659a6f7
elinsauer1 <elinsauer@hotmail.com>
2023-02-25 20:45:40 +0100

    Började arbeta på quizet

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 
 create_quiz_page();
+
+
+function random_number(max) {
+    return Math.floor(max * Math.random());
+} 
--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,7 @@
 function create_quiz_page(un_input) {
 
     document.querySelector("main").innerHTML = `
-    <div class="header_quiz">
+    <div class="header">
         <p>${un_input}</p>
         <button>logout</button>
     </div>
@@ -17,7 +17,47 @@ function create_quiz_page(un_input) {
     `
 
     document.querySelector("#wrapper").style.backgroundColor = "#c1afe4"
-    document.querySelector(".header_quiz button").addEventListener("click", create_login_page);
+    document.querySelector(".header button").addEventListener("click", button_action);
+
+    function button_action(event) {
+        //localStorage?
+        create_login_page();
+        document.querySelector("#wrapper").style.transition = "background-color 2s";
+    }
+
+
+
+    create_quiz();
+
+    async function create_quiz() {
+
+        let array_breeds = [];
+
+        while (array_breeds.length < 4) {
+            const dog_breed = ALL_BREEDS[random_number(ALL_BREEDS.length)];
+            console.log(dog_breed);
+            if (!array_breeds.includes(dog_breed)) {
+                array_breeds.push(dog_breed);
+            }
+        }
+
+        console.log(array_breeds);
+
+        const random_breed = array_breeds[random_number(array_breeds.length)];
+
+        console.log(random_breed);
+
+        const image_for_random_breed = await (await fetch_resource(`https://dog.ceo/api/breed/${random_breed.url}/images/random`)).json();
+
+        console.log(image_for_random_breed);
+
+        document.querySelector("#quiz_container img").src = await image_for_random_breed.message
+
+    }
+
+
+
+
 
 
 }
